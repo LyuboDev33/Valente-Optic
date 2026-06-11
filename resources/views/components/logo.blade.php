a3370bacff8ba7ece79033c0ad0bed9a7cf10c21
user <user@192.168.1.3>
2026-06-11 09:57:28 +0300

    First  commit

---
+++ resources/views/components/logo.blade.php
@@ -0,0 +1,9 @@
+@props([
+    'width' => 120
+])
+
+<img
+    width="{{ $width }}"
+    src="{{ asset('/assets/images/logo-valente.png') }}"
+    alt="Valente Logo"
+>
---
+++ resources/views/layouts/front-end.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    @yield('SEO')
+
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/assets/css/animate.min.css" />
+    <link rel="stylesheet" href="/assets/css/custom-animate.css" />
+    <link rel="stylesheet" href="/assets/css/swiper.min.css" />
+    <link rel="stylesheet" href="/assets/css/font-awesome-all.css" />
+    <link rel="stylesheet" href="/assets/css/jarallax.css" />
+    <link rel="stylesheet" href="/assets/css/jquery.magnific-popup.css" />
+    <link rel="stylesheet" href="/assets/css/flaticon.css" />
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="/assets/css/odometer.min.css" />
+    <link rel="stylesheet" href="/assets/css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="/assets/css/nice-select.css" />
+    <link rel="stylesheet" href="/assets/css/jquery-ui.css" />
+    <link rel="stylesheet" href="/assets/css/aos.css" />
+
+    <link rel="stylesheet" href="/assets/css/module-css/slider.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/footer.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/counter.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/services.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/about.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/brand.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/gallery.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/faq.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/testimonial.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/team.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/contact.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/pricing.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/blog.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/sliding-text.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/cta.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/feature.css" />
+    <link rel="stylesheet" href="/assets/css/module-css/banner.css" />
+
+    <!-- template styles -->
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/css/responsive.css" />
+
+
+</head>
+
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+
+
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+</body>
+
+
+
+
+<script src="/assets/js/jquery-3.6.0.min.js"></script>
+<script src="/assets/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/js/jarallax.min.js"></script>
+<script src="/assets/js/jquery.ajaxchimp.min.js"></script>
+<script src="/assets/js/jquery.appear.min.js"></script>
+<script src="/assets/js/swiper.min.js"></script>
+<script src="/assets/js/jquery.circle-progress.min.js"></script>
+<script src="/assets/js/jquery.magnific-popup.min.js"></script>
+<script src="/assets/js/isotope.js"></script>
+<script src="/assets/js/jquery.validate.min.js"></script>
+<script src="/assets/js/wNumb.min.js"></script>
+<script src="/assets/js/wow.js"></script>
+<script src="/assets/js/owl.carousel.min.js"></script>
+<script src="/assets/js/jquery-ui.js"></script>
+<script src="/assets/js/odometer.min.js"></script>
+<script src="/assets/js/jquery.nice-select.min.js"></script>
+<script src="/assets/js/jquery-sidebar-content.js"></script>
+<script src="/assets/js/marquee.min.js"></script>
+<script src="/assets/js/gsap/gsap.js"></script>
+<script src="/assets/js/gsap/ScrollTrigger.js"></script>
+<script src="/assets/js/gsap/SplitText.js"></script>
+<script src="/assets/js/aos.js"></script>
+
+<!-- template js -->
+<script src="/assets/js/script.js"></script>
+
+</html>
